3a7f4c457a39b0c4b8ababf8c325a4cc107d30c3
ibesaldev <adrien.juguet@lilo.org>
2025-01-17 18:43:05 +0100

    refactor(Game): use ParameterizedTest to test Game

--- src/test/java/domain/game/GameTest.java
+++ src/test/java/domain/game/GameTest.java
@@ -3,76 +3,46 @@ package domain.game;
 import domain.Result;
 import domain.Symbol;
 import org.assertj.core.api.ObjectAssert;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameTest {
-    
-    @Test
-    void rockShouldWinAgainstScissors() {
-        Player player1 = new Player(Symbol.ROCK);
-        Player player2 = new Player(Symbol.SCISSORS);
-
-        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
-        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
-    }
-
-    @Test
-    void paperShouldWinAgainstRock() {
-        Player player1 = new Player(Symbol.PAPER);
-        Player player2 = new Player(Symbol.ROCK);
-
-        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
-        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
-    }
-
-    @Test
-    void paperShouldWinAgainstWell() {
-        Player player1 = new Player(Symbol.PAPER);
-        Player player2 = new Player(Symbol.WELL);
-
-        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
-        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
-    }
-
-    @Test
-    void scissorsShouldWinAgainstPaper() {
-        Player player1 = new Player(Symbol.SCISSORS);
-        Player player2 = new Player(Symbol.PAPER);
-
-        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
-        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
-    }
-
-    @Test
-    void wellShouldWinAgainstRock() {
-        Player player1 = new Player(Symbol.WELL);
-        Player player2 = new Player(Symbol.ROCK);
-
-        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
-        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
-    }
-
-    @Test
-    void wellShouldWinAgainstScissors() {
-        Player player1 = new Player(Symbol.WELL);
-        Player player2 = new Player(Symbol.SCISSORS);
-
-        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
-        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
-    }
 
     @ParameterizedTest
     @EnumSource(Symbol.class)
-    void sameSymbolShouldReturnNull(Symbol symbol) {
+    void sameSymbolShouldReturnTie(Symbol symbol) {
         Player player1 = new Player(symbol);
         Player player2 = new Player(symbol);
 
         assertIsTie(new Game(player1, player2).play());
         assertIsTie(new Game(player2, player1).play());
+    }
+
+    @ParameterizedTest
+    @MethodSource("games")
+    void shouldReturnWinner(Symbol symbol1, Symbol symbol2) {
+        Player player1 = new Player(symbol1);
+        Player player2 = new Player(symbol2);
+
+        assertWinner(new Game(player1, player2).play()).isEqualTo(player1);
+        assertWinner(new Game(player2, player1).play()).isEqualTo(player1);
+    }
+
+    static Stream<Arguments> games() {
+        return Stream.of(
+                Arguments.of(Symbol.ROCK, Symbol.SCISSORS),
+                Arguments.of(Symbol.SCISSORS, Symbol.PAPER),
+                Arguments.of(Symbol.PAPER, Symbol.ROCK),
+                Arguments.of(Symbol.PAPER, Symbol.WELL),
+                Arguments.of(Symbol.WELL, Symbol.ROCK),
+                Arguments.of(Symbol.WELL, Symbol.SCISSORS)
+        );
     }
 
     private void assertIsTie(Result<Player> result) {
