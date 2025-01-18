b5523cde0dca3a5b1b2e6c4d3c43fe5ee9dcb98b
ibesaldev <adrien.juguet@lilo.org>
2025-01-18 03:25:37 +0100

    feature(Main): update main to generate a report from all possible strategies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,32 @@
+import domain.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+
+    public static final char FRAMACALC_DATA_SEPARATOR = '\t';
+    public static final char FRAMACALC_DECIMAL_SEPARATOR = '.';
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (ReportWriter report = ReportWriter
+                .writer(new FileWriter("game_results.txt"))
+                .dataSeparator(FRAMACALC_DATA_SEPARATOR)
+                .decimalSeparator(FRAMACALC_DECIMAL_SEPARATOR)
+                .symbolListSeparator('|')
+                .symbolListStartAndEnd('(', ')')
+                .build()) {
+            report.reportHeader();
+
+            for (Strategy strategy1 : Strategies.STRATEGIES) {
+                for (Strategy strategy2 : Strategies.STRATEGIES) {
+                    if (!strategy1.equals(strategy2)) {
+                        report.report(new GameLoop(strategy1, strategy2).run(10000));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/Strategies.java
@@ -0,0 +1,26 @@
+import domain.Strategy;
+
+import java.util.List;
+
+import static domain.Symbol.*;
+
+public final class Strategies {
+    public final static List<Strategy> STRATEGIES = List.of(
+            new Strategy(List.of(ROCK, PAPER, SCISSORS, WELL)),
+            new Strategy(List.of(PAPER, SCISSORS, WELL)),
+            new Strategy(List.of(ROCK, PAPER, SCISSORS)),
+            new Strategy(List.of(ROCK, PAPER, WELL)),
+            new Strategy(List.of(ROCK, SCISSORS, WELL)),
+            new Strategy(List.of(PAPER, WELL)),
+            new Strategy(List.of(ROCK, PAPER)),
+            new Strategy(List.of(ROCK, SCISSORS)),
+            new Strategy(List.of(ROCK, WELL)),
+            new Strategy(List.of(PAPER, SCISSORS)),
+            new Strategy(List.of(SCISSORS, WELL)),
+            new Strategy(List.of(ROCK)),
+            new Strategy(List.of(PAPER)),
+            new Strategy(List.of(SCISSORS)),
+            new Strategy(List.of(WELL))
+    );
+
+}
